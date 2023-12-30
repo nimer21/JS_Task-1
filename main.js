6d1f4c989562c2a588d2c753de139ca61c4371fa
Alsayed Nimer <nimer.alsayed@sos-palestine.org>
2023-12-30 08:41:01 +0200

    Task1 JS, Add/Delete CSS class using JS

---
+++ main.js
@@ -0,0 +1,6 @@
+document.querySelector("div").classList.add("demo");
+console.log(document.querySelector("div"));
+
+document.querySelector("h1").classList.remove("vip");
+console.log(document.querySelector("h1"));
+
